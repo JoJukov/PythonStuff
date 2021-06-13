6b479306d00abc22c402017fdab039c2e05454c5
JoJukov <ivanzhukov2002@mail.ru>
2021-06-13 21:12:56 +0300

    upd: second model - adding alpha handle

--- Physics-3s-2model/task1/F_Y_L_M.py
+++ Physics-3s-2model/task1/F_Y_L_M.py
@@ -3,8 +3,8 @@ from task1 import F_A_L_M, F_P_L_M
 
 
 def y_l_m(l: float, m: float):
-    return lambda theta: F_A_L_M.a_l_m(l, m) * F_P_L_M.p_l_m(l, m)(theta) * exp((-1) ** 0.5 * m * theta)
+    return lambda theta, alpha: F_A_L_M.a_l_m(l, m) * F_P_L_M.p_l_m(l, m)(theta) * exp((-1) ** 0.5 * m * alpha)
 
 
 def y_l_m_sq(l: float, m: float):
-    return lambda theta: abs(y_l_m(l, m)(theta)) * abs(y_l_m(l, m)(theta))
+    return lambda theta, alpha: abs(y_l_m(l, m)(theta, alpha)) * abs(y_l_m(l, m)(theta, alpha))
--- Physics-3s-2model/task1/plot.py
+++ Physics-3s-2model/task1/plot.py
@@ -6,7 +6,7 @@ import task1.F_Y_L_M
 
 def get_plot_by_l_m_sq(l: float, m: float):
     f = task1.F_Y_L_M.y_l_m_sq(l, m)
-    v = [f(i / 2) for i in range(720)]
+    v = [f(i / 2, 0) for i in range(720)]
 
     theta = np.arange(0., 2., 1. / 360.) * np.pi
     plt.polar(theta, v)
