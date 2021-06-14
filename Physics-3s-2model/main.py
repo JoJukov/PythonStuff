33c7ee0242cda67329e705194ea6db561359b715
JoJukov <ivanzhukov2002@mail.ru>
2021-06-14 16:50:04 +0300

    upd: added 3v plot

--- Physics-3s-2model/main.py
+++ Physics-3s-2model/main.py
@@ -1,4 +1,4 @@
 import task1.plot
 
 if __name__ == '__main__':
-    task1.plot.get_plot_by_l_m_sq(2, 0)
+    task1.plot.get_plot3_by_l_m_sq(4, 0)
--- Physics-3s-2model/task1/plot.py
+++ Physics-3s-2model/task1/plot.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
+import scipy.special as sp
 import numpy as np
 
 import task1.F_Y_L_M
 
 
-def get_plot_by_l_m_sq(l: float, m: float):
+def get_plot2_by_l_m_sq(l: float, m: float):
     f = task1.F_Y_L_M.y_l_m_sq(l, m)
     v = [f(i / 2, 0) for i in range(720)]
 
@@ -13,10 +14,27 @@ def get_plot_by_l_m_sq(l: float, m: float):
     plt.show()
 
 
-def get_plot_by_l_m(l: float, m: float):
+def get_plot2_by_l_m(l: float, m: float):
     f = task1.F_Y_L_M.y_l_m(l, m)
-    v = [f(i / 2) for i in range(720)]
+    v = [f(i / 2, 0) for i in range(720)]
 
     theta = np.arange(0., 2., 1. / 360.) * np.pi
     plt.polar(theta, v)
     plt.show()
+
+
+def get_plot3_by_l_m_sq(l: float, m: float):
+    theta, phi = np.meshgrid(np.linspace(0, 2 * np.pi, 90), np.linspace(0, np.pi, 90))
+    xyz = np.array([np.sin(theta) * np.sin(phi),
+                    np.sin(theta) * np.cos(phi),
+                    np.cos(theta)])
+    yy = sp.sph_harm(abs(m), l, phi, theta)
+    x, y, z = np.abs(yy) * xyz
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection='3d')
+    plot = ax.plot_surface(
+        x, y, z, rstride=1, cstride=1, cmap=plt.get_cmap('gnuplot2'),
+        linewidth=0, antialiased=False, alpha=0.5)
+
+    plt.show()
+
